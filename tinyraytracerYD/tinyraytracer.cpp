e4465e1934e969af11c4107589c34256c2f953b3
douysu <yindou97@163.com>
2020-05-23 19:29:21 -0700

    [add] 多球

--- tinyraytracerYD/tinyraytracer.cpp
+++ tinyraytracerYD/tinyraytracer.cpp
@@ -5,6 +5,7 @@
 // @version 1 20200519 绘制图像
 // @version 2 20200519 添加圆数据结构
 // @version 3 20200522 检测相交
+// @version 4 20200524 添加多个圆
 
 #include <limits>
 #include <cmath>
@@ -13,11 +14,23 @@
 #include <vector>
 #include "geometry.h"
 
+using namespace std;
+
+struct Material
+{
+    Vec3f diffuse_color;
+
+    Material(const Vec3f& color) : diffuse_color(color){}
+    Material() : diffuse_color(){}
+};
+
+
 struct Sphere {
     Vec3f center;
     float radius;
+    Material material;
 
-    Sphere(const Vec3f &c, const float &r) : center(c), radius(r) {}
+    Sphere(const Vec3f &c, const float &r, const Material& m) : center(c), radius(r), material(m) {}
 
     bool ray_intersect(const Vec3f &orig, const Vec3f &dir, float &t0) const {
         //  点c在射线上的投影是pc，向量以v开头
@@ -37,20 +50,43 @@ struct Sphere {
     }
 };
 
-Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const Sphere &sphere) {
-    float sphere_dist = std::numeric_limits<float>::max();
-    if (!sphere.ray_intersect(orig, dir, sphere_dist)) {
+bool scene_intersect(const Vec3f& orig, const Vec3f& dir, const vector<Sphere>& spheres, Vec3f& hit, Vec3f& N, Material& material)
+{
+    float spheres_dist = numeric_limits<float>::max();
+    for (size_t i = 0; i < spheres.size(); i++)
+    {
+        float dist_i;
+        if(spheres[i].ray_intersect(orig, dir, dist_i) && dist_i < spheres_dist)
+        {
+            spheres_dist = dist_i;
+            // 计算投影点
+            hit = orig + dir * dist_i;
+            // 计算圆中心与投影的向量
+            N = (hit - spheres[i].center).normalize();
+            material = spheres[i].material;
+        }
+    }
+    
+    return spheres_dist < 1000;
+}
+
+Vec3f cast_ray(const Vec3f &orig, const Vec3f &dir, const vector<Sphere>& spheres) {
+    Vec3f point, N;
+    Material material;
+
+    if (!scene_intersect(orig, dir, spheres, point, N, material))
+    {
         return Vec3f(0.2, 0.7, 0.8); // 背景色
     }
 
-    return Vec3f(0.4, 0.4, 0.3);
+    return material.diffuse_color;
 }
 
-void render(const Sphere &sphere) {
+void render(const vector<Sphere>& spheres) {
     const int width    = 1024;
     const int height   = 768;
     const int fov      = M_PI/2.;
-    std::vector<Vec3f> framebuffer(width*height);
+    vector<Vec3f> framebuffer(width*height);
 
     #pragma omp parallel for
     for (size_t j = 0; j<height; j++) {
@@ -61,25 +97,33 @@ void render(const Sphere &sphere) {
             float x =  (2*(i + 0.5)/(float)width  - 1) * tan(fov/2.) * width / (float)height;
             float y = -(2*(j + 0.5)/(float)height - 1) * tan(fov/2.);
             Vec3f dir = Vec3f(x, y, -1).normalize();
-            framebuffer[i+j*width] = cast_ray(Vec3f(0,0,0), dir, sphere);
+            framebuffer[i+j*width] = cast_ray(Vec3f(0,0,0), dir, spheres);
         }
     }
 
-    std::ofstream ofs; // save the framebuffer to file
+    ofstream ofs; // save the framebuffer to file
     ofs.open("./out.ppm");
     ofs << "P6\n" << width << " " << height << "\n255\n";
     for (size_t i = 0; i < height*width; ++i) {
         for (size_t j = 0; j<3; j++) {
 			// camera为(0, 0, 0)，方向向量标准化
-            ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
+            ofs << (char)(255 * max(0.f, min(1.f, framebuffer[i][j])));
         }
     }
     ofs.close();
 }
 
-int main() {
-    Sphere sphere(Vec3f(-3, 0, -16), 2);
-    render(sphere);
+int main(int argc, char** argv) {
+    Material ivory(Vec3f(0.4, 0.4, 0.3)); // 象牙白
+    Material red_rubber(Vec3f(0.3, 0.1, 0.1)); // 红橡胶
+    
+    vector<Sphere> spheres;
+    spheres.push_back(Sphere(Vec3f(-3,    0,   -16), 2,      ivory));
+    spheres.push_back(Sphere(Vec3f(-1.0, -1.5, -12), 2, red_rubber));
+    spheres.push_back(Sphere(Vec3f( 1.5, -0.5, -18), 3, red_rubber));
+    spheres.push_back(Sphere(Vec3f( 7,    5,   -18), 4,      ivory));
+    
+    render(spheres);
 
     return 0;
 }
